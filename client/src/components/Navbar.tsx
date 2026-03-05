9956d85a04dea1bd415c93dca41660d70da526fe
GuoYangtuo <179408019@qq.com>
2026-03-06 03:02:34 +0800

    热力图显示bug改进

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -467,7 +467,7 @@ const Navbar: React.FC = () => {
                             minWidth: 180,
                             overflow: 'visible',
                             filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                            '&:before': {
+                            /*'&:before': {
                               content: '""',
                               display: 'block',
                               position: 'absolute',
@@ -478,7 +478,7 @@ const Navbar: React.FC = () => {
                               bgcolor: 'background.paper',
                               transform: 'translateY(-50%) rotate(45deg)',
                               zIndex: 0,
-                            },
+                            },*/
                           }}
                         >
                           <MenuList autoFocusItem={false}>
